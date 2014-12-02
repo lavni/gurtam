e3a0f9961f9ae74fac4a3d96b9c68ca5b6ff317b
lavni <lavniii@gmail.com>
2014-12-03 00:58:20 +0300

    added keyboard events

--- smiles/script.js
+++ smiles/script.js
@@ -14,18 +14,39 @@ function setIcon(i){
 
 
 
-leftButton = document.getElementById("left");
-leftButton.onclick = function() {
+// leftButton = document.getElementById("left");
+// leftButton.onclick = function() {
+//   var ind =  document.getElementById("icon").getAttribute("ind");
+//   ind--;
+//   setIcon(ind);
+//   document.getElementById("icon").setAttribute("ind", ind);
+// };
+
+function leftClick(){
   var ind =  document.getElementById("icon").getAttribute("ind");
   ind--;
   setIcon(ind);
   document.getElementById("icon").setAttribute("ind", ind);
-};
-
-rightButton = document.getElementById("right");
-rightButton.onclick = function() {
+}
+function rightClick(){
   var ind =  document.getElementById("icon").getAttribute("ind");
   ind++;
   setIcon(ind);
   document.getElementById("icon").setAttribute("ind", ind);
-};
+}
+
+leftButton = document.getElementById("left");
+leftButton.onclick = leftClick;
+
+rightButton = document.getElementById("right");
+rightButton.onclick = rightClick;
+
+function handler(event) {
+  if (event.keyCode == 37) {
+    leftClick();
+  } else if (event.keyCode == 39) {
+    rightClick();
+  }
+}
+window.addEventListener('keydown', handler, false);
+
